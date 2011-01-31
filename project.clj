5b5ffb76f900ed3a6239d6f344daee5df708f865
Eugene Koontz <ekoontz@hiro-tan.org>
2011-01-30 23:43:43 -0800

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject italianverbs "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [slice "0.4.0-SNAPSHOT"]
+                 [org.apache.lucene/lucene-core "3.0.2"]
+                 [org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
---
+++ src/italianverbs/core.clj
@@ -0,0 +1,1 @@
+(ns italianverbs.core)
